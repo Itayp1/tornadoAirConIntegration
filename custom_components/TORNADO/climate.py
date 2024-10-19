436a22a1ebc795b2cd4dbd7373639f036acc0bc1
unknown <{ID}+{username}@users.noreply.github.com>
2024-10-19 19:59:04 +0300

    add climate

--- custom_components/TORNADO/climate.py
+++ custom_components/TORNADO/climate.py
@@ -33,81 +33,43 @@ class TornadoClimat(TornadoEntity ,ClimateEntity):
     def __init__(self , cord ,entry):
         """Initialize the climate device."""
         super().__init__(cord,entry)        
-        self._name = "My Climate Device"
-        self._hvac_mode = HVAC_MODE_OFF
-        self._current_temperature = 26.0
-        self._target_temperature = 25.0
-        self._temperature_unit = TEMP_CELSIUS
-        self._fan_mode = "auto"
-        self._preset_mode = None
-        self._hvac_modes = [HVAC_MODE_OFF, HVAC_MODE_HEAT, HVAC_MODE_COOL, HVAC_MODE_AUTO]
-        self._fan_modes = ["auto", "silent", "low", "medium", "high"]
-        self._coordinator = cord
-        self._entry = entry
+        self._attr_name = "My Climate Device"
+        self._attr_hvac_mode = HVAC_MODE_OFF
+        self._attr_current_temperature = 26.0
+        self._attr_target_temperature = 25.0
+        self._attr_temperature_unit = TEMP_CELSIUS
+        self._attr_fan_mode = "auto"
+        self._attr_preset_mode = None
+        self._attr_hvac_modes = [HVAC_MODE_OFF, HVAC_MODE_HEAT, HVAC_MODE_COOL, HVAC_MODE_AUTO]
+        self._attr_fan_modes = ["auto", "silent", "low", "medium", "high"]
+        self._attr_coordinator = cord
+        self._attr_entry = entry
         self._attr_preset_modes = ['ECO' ,'NORMAL']
-    @property
-    def name(self):
-        """Return the name of the climate device."""
-        return self._name
+        self._attr_supported_features =  [
+            SUPPORT_TARGET_TEMPERATURE,
+            SUPPORT_FAN_MODE,
+            SUPPORT_PRESET_MODE
+        ]
+ 
 
-    @property
-    def temperature_unit(self):
-        """Return the unit of measurement."""
-        return self._temperature_unit
-
-    @property
-    def current_temperature(self):
-        """Return the current temperature."""
-        return self._current_temperature
-
-    @property
-    def target_temperature(self):
-        """Return the temperature we try to reach."""
-        return self._target_temperature
-
-    @property
-    def hvac_mode(self):
-        """Return the current operation mode (HVAC mode)."""
-        return self._hvac_mode
-
-    @property
-    def hvac_modes(self):
-        """Return the list of available HVAC operation modes."""
-        return self._hvac_modes
-
-    @property
-    def supported_features(self):
-        """Return the list of supported features."""
-        return SUPPORT_TARGET_TEMPERATURE | SUPPORT_FAN_MODE | SUPPORT_PRESET_MODE 
-    
-
-    @property
-    def fan_mode(self):
-        """Return the current fan mode."""
-        return self._fan_mode
-
-    @property
-    def fan_modes(self):
-        """Return the list of available fan modes."""
-        return self._fan_modes
 
 
     async def async_set_temperature(self, **kwargs):
         """Set new target temperature."""
         temperature = kwargs.get(ATTR_TEMPERATURE)
         if temperature is not None:
-            self._target_temperature = temperature
+            self._attr_target_temperature = temperature
             self.async_write_ha_state()
 
     async def async_set_hvac_mode(self, hvac_mode):
         """Set the HVAC mode."""
-        if hvac_mode in self._hvac_modes:
+        if hvac_mode in self._attr_hvac_mode:
             self._hvac_mode = hvac_mode
             self.async_write_ha_state()
 
     async def async_set_fan_mode(self, fan_mode):
         """Set the fan mode."""
-        if fan_mode in self._fan_modes:
+        if fan_mode in self._attr_fan_modes:
             self._fan_mode = fan_mode
             self.async_write_ha_state()
 
